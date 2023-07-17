9de70dff57fea21bcc9d4bb819b2cc7b167012d6
Baguette-Enjoyer <klong31122001@gmail.com>
2023-07-17 20:06:58 +0700

    restructured

--- src/firebase/firebaseApp.js
+++ src/firebase/firebaseApp.js
@@ -1,5 +1,5 @@
 import admin from 'firebase-admin'
-import serviceKey from '../getgo-461d2-6df7eba352ca.json'
+import serviceKey from '../../getgo-461d2-6df7eba352ca.json'
 
 const firebaseApp = admin.initializeApp({
     credential: admin.credential.cert(serviceKey)
