529fe3bbc48eca83a1dd35c1d11a3d52c3cfb3db
Baguette-Enjoyer <klong31122001@gmail.com>
2023-07-17 21:40:15 +0700

    adjust things

--- src/firebase/firebaseApp.js
+++ src/firebase/firebaseApp.js
@@ -1,8 +1,8 @@
-import admin from 'firebase-admin'
-import serviceKey from '../../getgo-461d2-6df7eba352ca.json'
+// import admin from 'firebase-admin'
+// import serviceKey from '../../getgo-461d2-6df7eba352ca.json'
 
-const firebaseApp = admin.initializeApp({
-    credential: admin.credential.cert(serviceKey)
-})
+// const firebaseApp = admin.initializeApp({
+//     credential: admin.credential.cert(serviceKey)
+// })
 
-export default firebaseApp
+// export default firebaseApp
