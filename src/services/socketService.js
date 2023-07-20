621542fb1e08e6b0487451f4aa54c162e5ba2d72
Baguette-Enjoyer <klong31122001@gmail.com>
2023-07-20 19:59:28 +0700

    ...

--- src/services/socketService.js
+++ src/services/socketService.js
@@ -6,7 +6,7 @@ let trips = new Map()
 let current_intervals = new Map()
 
 let initSocket = (io) => {
-    io.of('getsgosocket').on('connection', (socket) => {
+    io.on('connection', (socket) => {
         console.log("socket " + socket.id + " connected")
         handleUserLogin(socket)
         handleDriverLogin(socket)
