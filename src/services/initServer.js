a9ade5f28336301c90c1dd733b55b5ab60481d35
Baguette-Enjoyer <klong31122001@gmail.com>
2023-07-21 09:45:27 +0700

    ...

--- src/services/initServer.js
+++ src/services/initServer.js
@@ -1,7 +1,7 @@
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
-import initServerRoutes from "./routes/serverRoutes";
+import initServerRoutes from "../routes/serverRoutes";
 
 const app = express()
 const server = http.createServer(app)
--- src/services/userService.js
+++ src/services/userService.js
@@ -5,44 +5,46 @@ import jwtService from './jwtService';
 
 const salt = bcrypt.genSaltSync(10);
 
-let RegisterUser = async (data) =>{
+
+
+let RegisterUser = async (data) => {
   return new Promise(async (resolve, reject) => {
     console.log(data)
     let phone = data.phone
     let checkUser = await GetUserByPhone(phone)
-    if (checkUser != null){
+    if (checkUser != null) {
       return resolve({
-        statusCode:400,
-        message:"Phone existed",
+        statusCode: 400,
+        message: "Phone existed",
       })
     }
-  
+
     let pwd = data.password
     let hash = await hashUserPassword(pwd)
     try {
       let user = await db.User.create({
-        name:data.name,
-        phone:phone,
-        password:hash,
+        name: data.name,
+        phone: phone,
+        password: hash,
         gender: data.gender,
         birthday: new Date(),
         avatar: data.avatar,
-        active:true,
-        type:"User"
+        active: true,
+        type: "User"
       })
-      let token = await jwtService.GenerateAccessToken(user.id,phone,"User")
+      let token = await jwtService.GenerateAccessToken(user.id, phone, "User")
       await db.User.update({
-        accessToken:token
-      },{
+        accessToken: token
+      }, {
         where: {
           id: user.id
         }
       })
       return resolve({
-        statusCode:200,
-        message:"User created",
-        phone:phone,
-        accessToken:token,
+        statusCode: 200,
+        message: "User created",
+        phone: phone,
+        accessToken: token,
       })
     } catch (error) {
       reject(new Error("error registering"))
@@ -52,26 +54,26 @@ let RegisterUser = async (data) =>{
   })
 }
 
-let LoginUser = async(data)=>{
-  return new Promise(async (resolve, reject) => { 
+let LoginUser = async (data) => {
+  return new Promise(async (resolve, reject) => {
     let phone = data.phone
-    let password = data.password 
+    let password = data.password
     let checkUser = await GetUserByPhone(phone)
-    if (checkUser == null){
+    if (checkUser == null) {
       return resolve({
-        statusCode:500,
-        message:"Phone not exist",
+        statusCode: 500,
+        message: "Phone not exist",
       })
     }
     let hashPassword = checkUser.password
-    let result = await comparePassword(password,hashPassword)
-    if (!result){
+    let result = await comparePassword(password, hashPassword)
+    if (!result) {
       return resolve({
-      statusCode:500,
-      message:"wrong password",
+        statusCode: 500,
+        message: "wrong password",
       })
     }
-    let token = checkUser.accessToken 
+    let token = checkUser.accessToken
     // if (token == null){
     //   token = await jwtService.GenerateAccessToken(checkUser.id,checkUser.phone,checkUser.type)
     // await db.User.update({
@@ -83,75 +85,75 @@ let LoginUser = async(data)=>{
     //   })
     // }
     let verifyToken = await jwtService.VerifyToken(token)
-    if (verifyToken.result == false || token == null){
-        token = await jwtService.GenerateAccessToken(checkUser.id,checkUser.phone,checkUser.type)
-        await db.User.update({
-            accessToken:token
-          },
-          {
-            where: {
-              id: checkUser.id
-            }
-          })
-    }   
-    
+    if (verifyToken.result == false || token == null) {
+      token = await jwtService.GenerateAccessToken(checkUser.id, checkUser.phone, checkUser.type)
+      await db.User.update({
+        accessToken: token
+      },
+        {
+          where: {
+            id: checkUser.id
+          }
+        })
+    }
+
     return resolve({
-      statusCode:200,
-      user_info:{
-        user_id:checkUser.id,
-        phone:checkUser.phone,
-        type:checkUser.type,
-        accessToken:token,
+      statusCode: 200,
+      user_info: {
+        user_id: checkUser.id,
+        phone: checkUser.phone,
+        type: checkUser.type,
+        accessToken: token,
       }
     })
   })
 }
 
-let GetUserByPhone = async (phone) =>{
-    return new Promise(async (resolve, reject) => { 
-        let user = await db.User.findOne({
-            where: {
-                phone: phone,
-            }
-        })
-        if (user){
-          resolve(user)
-        }
-        else {
-          resolve(null)
-        }
+let GetUserByPhone = async (phone) => {
+  return new Promise(async (resolve, reject) => {
+    let user = await db.User.findOne({
+      where: {
+        phone: phone,
+      }
     })
+    if (user) {
+      resolve(user)
+    }
+    else {
+      resolve(null)
+    }
+  })
 }
 
-let GetUserById = async (user_id) =>{
-  return new Promise(async (resolve, reject) => { 
+let GetUserById = async (user_id) => {
+  return new Promise(async (resolve, reject) => {
     let user = db.User.findOne({
-      where: {id:user_id}
+      where: { id: user_id }
     })
-    if (user){
-          resolve(user)
-        }
-        else {
-          resolve(null)
-        } 
+    if (user) {
+      resolve(user)
+    }
+    else {
+      resolve(null)
+    }
   })
 }
 
 
 let hashUserPassword = (password) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            let hashPassWord = await bcrypt.hashSync(password, salt);
-            resolve(hashPassWord);
-        } catch (e) {
-            reject(e);
-        }
-    });
+  return new Promise(async (resolve, reject) => {
+    try {
+      let hashPassWord = await bcrypt.hashSync(password, salt);
+      resolve(hashPassWord);
+    } catch (e) {
+      reject(e);
+    }
+  });
 };
 
-let comparePassword = (password,hash) =>{
-  return new Promise(async (resolve, reject) => { 
-    let result = await bcrypt.compare(password,hash)
+let comparePassword = (password, hash) => {
+  return new Promise(async (resolve, reject) => {
+    let result = await bcrypt.compare(password, hash)
     resolve(result)
   })
 }
