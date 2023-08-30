ebd0c811f24af3cb5fb686c380bfc22416007e68
Baguette-Enjoyer <klong31122001@gmail.com>
2023-08-30 13:53:25 +0700

    attempt02

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -7,14 +7,7 @@ const DB_PWD = process.env.DB_PWD
 let sequelize = new Sequelize(DB_NAME, DB_USN, DB_PWD, {
   host: process.env.HOST,
   dialect: "mysql",
-  "port": process.env.DB_PORT,
-  "dialectOptions": {
-    // "ssl": {
-    //   "required": false,
-    //   "rejectUnauthorized": false
-    // }
-    "ssl": false
-  },
+  // "port": process.env.DB_PORT,
 
 });
 // let sequelize_dev = new Sequelize(DB_NAME, DB_USN, DB_PWD, {
