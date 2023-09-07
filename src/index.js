eedaa8254b25cda045540177bb97658ad158011e
Baguette-Enjoyer <klong31122001@gmail.com>
2023-09-07 22:22:41 +0700

    command

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ initializeApp({
 initServer.getServer().listen(process.env.PORT || 3000, () => {
   console.log("server listening on 3000")
 })
-sendMessageFirebase('dr4WpNi_QL6IKES0U5qlcr:APA91bGwdTlVQbjWer2BLakMWd4GCXMkgq0vR24bYyL8EjSW9VYBTnUo81-r2zS2KHhMh1lsLyQYse8SibCmut7cU4giP3nAcYxSwNOGzdSRV8Sc0WM26h7nE7NYeXmzqqMjc3xJut95', 'Chuyến đi hẹn giờ', "Đã có tài xế chấp nhận")
+// sendMessageFirebase('dr4WpNi_QL6IKES0U5qlcr:APA91bGwdTlVQbjWer2BLakMWd4GCXMkgq0vR24bYyL8EjSW9VYBTnUo81-r2zS2KHhMh1lsLyQYse8SibCmut7cU4giP3nAcYxSwNOGzdSRV8Sc0WM26h7nE7NYeXmzqqMjc3xJut95', 'Chuyến đi hẹn giờ', "Đã có tài xế chấp nhận")
 
 process.on("beforeExit", () => {
   rd.quit();
